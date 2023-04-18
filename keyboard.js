101df549459648a44517968cd3a7daf1c318d3e3
Sweet-ka <sweet-ka@mail.ru>
2023-04-18 16:44:12 +0300

    feat: begin add class

---
+++ keyboard.js
@@ -0,0 +1,24 @@
+export const arr = [
+    {
+      width: 100,
+      height: 100,
+      x: 10,
+      y: 10,
+      x_shadow: 0,
+      y_shadow: 0,
+      x_delta: 0,
+      y_delta: 0,
+      code: "KeyZ"
+    },
+    {
+      width: 100,
+      height: 100,
+      x: 1000,
+      y: 10,
+      x_shadow: 0,
+      y_shadow: 0,
+      x_delta: 0,
+      y_delta: 0,
+      code: "KeyX"
+    },
+  ];
--- test.js
+++ test.js
@@ -1,60 +1,39 @@
-const canvas = document.getElementById("can");
-const ctx = canvas.getContext("2d");
+import { arr } from "./keyboard.js";
 
-let canvas_width = 1200
-let canvas_height = 500
-let delta_x_max = 5;
-let delta_y_max = 5;
-let inner = 0.1;
-let y_end;
-let r = 8;
+class Keyboard {
 
-let strokeStyle_button = "black"
-let fillStyle_button = "gray"
-let shadowColor_button = "black"
+  canvas_width = 1200
+  canvas_height = 500
+  delta_x_max = 5;
+  delta_y_max = 5;
+  inner = 0.1;
+  r = 8;
+  strokeStyle_button = "black"
+  fillStyle_button = "gray"
+  shadowColor_button = "black"
+
+  constructor(){
+    this.element = document.createElement("canvas")
+    
+    canvas.setAttribute("id", "can");
+    const ctx = canvas.getContext("2d");
 
 function set_delta_x(w, x){
-  return ((canvas_width-w)/2-x)/canvas_width*delta_x_max
+  return ((this.canvas_width-w)/2-x)/this.canvas_width*this.delta_x_max
 }
 
 function set_delta_y(h, y){
-  return (canvas_height-h-y)/canvas_height*delta_y_max
+  return (this.canvas_height-h-y)/this.canvas_height*this.delta_y_max
 }
-
-let arr = [
-  {
-    width: 100,
-    height: 100,
-    x: 10,
-    y: 10,
-    x_shadow: 0,
-    y_shadow: 0,
-    x_delta: 0,
-    y_delta: 0,
-    code: "KeyZ"
-  },
-  {
-    width: 100,
-    height: 100,
-    x: 1000,
-    y: 10,
-    x_shadow: 0,
-    y_shadow: 0,
-    x_delta: 0,
-    y_delta: 0,
-    code: "KeyX"
-  },
-];
-
 
 function art(el, x, y) {
 
-  ctx.strokeStyle = strokeStyle_button;
-  ctx.shadowColor = shadowColor_button;
+  ctx.strokeStyle = this.strokeStyle_button;
+  ctx.shadowColor = this.shadowColor_button;
 
   ctx.save();
 
-  ctx.fillStyle = fillStyle_button;
+  ctx.fillStyle = this.fillStyle_button;
   ctx.beginPath();
   ctx.shadowOffsetX = el.x_shadow - x;
   ctx.shadowOffsetY = el.y_shadow - y;
@@ -64,10 +43,10 @@ function art(el, x, y) {
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
   ctx.roundRect(
-    el.x + x + el.width * inner,
-    el.y + y + el.height * inner,
-    el.width * (1 - inner * 2),
-    el.height * (1 - inner * 2),
+    el.x + x + el.width * this.inner,
+    el.y + y + el.height * this.inner,
+    el.width * (1 - this.inner * 2),
+    el.height * (1 - this.inner * 2),
     [r]
   );
   ctx.stroke();
@@ -75,7 +54,7 @@ function art(el, x, y) {
   ctx.restore();
 }
 
-arr.forEach((item, index) => {
+this.arr.forEach((item, index) => {
   item.x_shadow = set_delta_x(item.width, item.x)
   item.y_shadow = set_delta_y(item.height, item.y)
   art(item, item.x_delta, item.y_delta);
@@ -118,7 +97,7 @@ const animate = function (i, delta) {
     arr[i].y_delta = progress_y;
 
     ctx.clearRect(0, 0, 1200, 500);
-    
+
     arr.forEach((item) => {
       art(item, item.x_delta, item.y_delta);
     });
@@ -139,7 +118,7 @@ let animate_back;
 
 addEventListener("mousedown", (e) => {
   let coord = get_coord(e);
-  arr.forEach((item, i) => {
+  this.arr.forEach((item, i) => {
     if (
       coord.ex >= item.x &&
       coord.ex <= item.x + item.width &&
@@ -163,3 +142,9 @@ addEventListener("mousedown", (e) => {
 function get_coord(e) {
   return { ex: e.clientX - canvas_x, ey: e.clientY - canvas_y };
 }
+
+}
+}
+
+const can = new Keyboard()
+document.body.append(can.element)
